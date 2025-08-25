d45e3f9f320ccc986d21711d187c70bf21b39eb9
Sagar Hooda <sagarhooda.uscan@gmail.com>
2025-08-25 19:56:44 -0400

    25

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 
 const habitablePlanets = [];
 
+function loadPlanetsData(){
+
+return new Promise((resolve, reject)=> { 
+
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
 }
-
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
     comment: '#',
@@ -21,13 +24,16 @@ fs.createReadStream('kepler_data.csv')
   })
   .on('error', (err) => {
     console.log(err);
+    reject(error)                                                          // rejecting if we receive any error
   })
   .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
     console.log(`${habitablePlanets.length} habitable planets found!`);
+    resolve();                                                              // calling it when we are done parsing the data
   });
 
 
-module.exports = { planets: habitablePlanets}
+})
+}
+
+
+module.exports = {loadPlanetData, planets: habitablePlanets}
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
 const app = require('./app')
 const PORT = process.env.PORT || 8000
 
+const {loadPlanetData} = require('./models/planets.model')
+
 const http = require('http')
 const server = http.createServer(app)
+
+await loadPlanetData();
 server.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
