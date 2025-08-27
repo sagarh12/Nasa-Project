21052035e8fe5549f4fb2c2e0375e066eb7a8332
Sagar Hooda <sagarhooda.uscan@gmail.com>
2025-08-27 15:47:52 -0400

    27

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,12 @@ const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const morgan = require('morgan')
 const planetsRouter = require('./routes/planets/planets.router')
 
 app.use(cors({origin: 'http://localhost:3000'}))
+app.use(morgan("combined"))
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
