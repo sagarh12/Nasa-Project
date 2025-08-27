7b3b2714e3319efa1a6681462a9b26168de505b3
Sagar Hooda <sagarhooda.uscan@gmail.com>
2025-08-26 23:39:09 -0400

    26

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,12 @@ const planetsRouter = require('./routes/planets/planets.router')
 
 app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json())
-app.use(planetsRouter)
 app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use(planetsRouter)
+app.get((req, res)=> {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
+
 
 module.exports = app
