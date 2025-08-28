aefca16ca6ab99f271f14380fed67cf2d2af1b44
Sagar Hooda <sagarhooda.uscan@gmail.com>
2025-08-27 21:29:32 -0400

    27

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const morgan = require('morgan')
+
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 
 app.use(cors({origin: 'http://localhost:3000'}))
 app.use(morgan("combined"))
@@ -12,6 +14,9 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(planetsRouter)
+app.use(launchesRouter)
+
+
 app.get((req, res)=> {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const {getAllLaunches} = require('./launches.controller')
+const launchesRouter = express.Router()
+
+launchesRouter.get('/', getAllLaunches)
+
+module.exports = launchesRouter   
